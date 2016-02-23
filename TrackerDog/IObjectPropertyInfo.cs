a88820362dcb4585289647ac610b28a1f02a7211
mfidemraizer <fidemraizer@hotmail.com>
2016-02-23 13:07:18 +0100

    New doc to "useful metadata" and increased NuGet package to 1.1.0

--- TrackerDog/IObjectPropertyInfo.cs
+++ TrackerDog/IObjectPropertyInfo.cs
@@ -3,10 +3,24 @@
     using System.Collections.Immutable;
     using System.Reflection;
 
+    /// <summary>
+    /// Defines a type which holds metadata about a given type property.
+    /// </summary>
     public interface IObjectPropertyInfo
     {
+        /// <summary>
+        /// Gets a list where each item is the owner association for the next one
+        /// </summary>
         IImmutableList<PropertyInfo> PathParts { get; }
+
+        /// <summary>
+        /// Gets a reference to the last property in the path.
+        /// </summary>
         PropertyInfo Property { get; }
+
+        /// <summary>
+        /// Gets a string, dot-separated representation of <see cref="PathParts"/>
+        /// </summary>
         string Path { get; }
     }
 }
