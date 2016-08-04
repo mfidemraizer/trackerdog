4163b45b9544d8fc2b830123eee17cb1c29ef357
Matías Fidemraizer <fidemraizer@hotmail.com>
2016-08-04 12:39:01 +0200

    Updated documentation
    Now documentation will reflect configuration model changes based on
    issue #12

--- TrackerDog/ITrackableObjectFactory.cs
+++ TrackerDog/ITrackableObjectFactory.cs
@@ -2,9 +2,21 @@
 {
     public interface ITrackableObjectFactory
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TObject"></typeparam>
+        /// <param name="args"></param>
+        /// <returns></returns>
         TObject CreateOf<TObject>(params object[] args)
                where TObject : class;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TObject"></typeparam>
+        /// <param name="some"></param>
+        /// <returns></returns>
         TObject CreateFrom<TObject>(TObject some)
             where TObject : class;
     }
